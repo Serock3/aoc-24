0ed798d98048b0445a0ebf08c1224924d4057489
Sebastian <sebastian.holmin@hotmail.com>
2024-12-11 21:27:30 +0100

    Clean up 11

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -3,40 +3,41 @@ use cached::proc_macro::cached;
 advent_of_code::solution!(11);
 
 pub fn part_one(input: &str) -> Option<u64> {
+    let num_blinks = 25;
+
+    solve(input, num_blinks)
+}
+
+fn solve(input: &str, num_blinks: u32) -> Option<u64> {
     let input = input
         .split_ascii_whitespace()
         .map(|x| x.parse::<u64>().unwrap());
-
-    let start = 25;
-    Some(input.map(|x| get_number_of_stones(x, start)).sum())
-}
-
-#[cached]
-fn get_number_of_stones(x: u64, start: i32) -> u64 {
-    if start == 0 {
-        return 1;
-    }
-    if x == 0 {
-        return get_number_of_stones(1, start - 1);
-    }
-    let num_digits = x.ilog10() + 1;
-    if num_digits % 2 == 0 {
-        let left_split = x / 10u64.pow(num_digits / 2);
-        let right_split = x % 10u64.pow(num_digits / 2);
-        return get_number_of_stones(left_split, start - 1)
-            + get_number_of_stones(right_split, start - 1);
-    } else {
-        get_number_of_stones(x * 2024, start - 1)
-    }
+    Some(input.map(|x| get_number_of_stones(x, num_blinks)).sum())
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let input = input
-        .split_ascii_whitespace()
-        .map(|x| x.parse::<u64>().unwrap());
+    let num_blinks = 75;
 
-    let start = 75;
-    Some(input.map(|x| get_number_of_stones(x, start)).sum())
+    solve(input, num_blinks)
+}
+
+#[cached]
+fn get_number_of_stones(stone_num: u64, blink: u32) -> u64 {
+    if blink == 0 {
+        return 1;
+    }
+    if stone_num == 0 {
+        return get_number_of_stones(1, blink - 1);
+    }
+    let num_digits = stone_num.ilog10() + 1;
+    if num_digits % 2 == 0 {
+        let left_split = stone_num / 10u64.pow(num_digits / 2);
+        let right_split = stone_num % 10u64.pow(num_digits / 2);
+        return get_number_of_stones(left_split, blink - 1)
+            + get_number_of_stones(right_split, blink - 1);
+    } else {
+        get_number_of_stones(stone_num * 2024, blink - 1)
+    }
 }
 
 #[cfg(test)]
@@ -47,11 +48,5 @@ mod tests {
     fn test_part_one() {
         let result = part_one("125 17");
         assert_eq!(result, Some(55312));
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
     }
 }
